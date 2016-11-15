beea198d43f1c4b6b1eca9ab1bb4dac93e01836a
Taylor <Taylor.Hutchinson@macys.com>
2016-11-15 14:14:26 -0800

    B-60214 Update tests

--- src/test/java/com/macys/sdt/framework/utils/db/utils/EnvironmentDetailsTest.java
+++ src/test/java/com/macys/sdt/framework/utils/db/utils/EnvironmentDetailsTest.java
@@ -1,5 +1,6 @@
 package com.macys.sdt.framework.utils.db.utils;
 
+import com.macys.sdt.framework.runner.MainRunner;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -7,16 +8,9 @@ public class EnvironmentDetailsTest {
 
     @Test
     public void gceEnvDetailsTest() {
-        final String WEBSITE = "http://mcom-1114.c4d.devops.fds.com/";
+        String WEBSITE = "http://mcom-1114.c4d.devops.fds.com/";
+        MainRunner.url = WEBSITE;
         String url = EnvironmentDetails.getServiceURL(WEBSITE);
         Assert.assertEquals(url, "http://c4d.devops.fds.com/reinfo/mcom-1114");
-    }
-
-    @Test
-    public void stage5EnvDetailsTest() {
-        final String WEBSITE = "http://www.qa17codemacys.fds.com/";
-        String url = EnvironmentDetails.getServiceURL(WEBSITE);
-        Assert.assertEquals(url,
-                "http://mdc2vr6133:8088/EnvironmentDetailsRestApi/environmentService/getNewEnvDetails/qa17codemacys");
     }
 }
