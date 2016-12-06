28755e522350f859efac67c8b156d786605ad33f
Mahendar Doosa <Mahendar.Doosa@macys.com>
2016-12-06 17:52:04 +0530

    B-60214 Models Unit Tests

--- src/test/java/com/macys/sdt/framework/model/ProfileAddressTest.java
+++ src/test/java/com/macys/sdt/framework/model/ProfileAddressTest.java
@@ -32,4 +32,134 @@ public class ProfileAddressTest {
         Assert.assertEquals("8904", phoneSubscriber);
     }
 
+    @Test
+    public void testGetProvince() throws Exception {
+        String province = "Alberta";
+        profileAddress.setProvince(province);
+        Assert.assertEquals(province, profileAddress.getProvince());
+    }
+
+    @Test
+    public void testGetId() throws Exception {
+        Long id = 1234L;
+        profileAddress.setId(id);
+        Assert.assertEquals(id, profileAddress.getId());
+    }
+
+    @Test
+    public void testGetAttention() throws Exception {
+        String attention = "testattention";
+        profileAddress.setAttention(attention);
+        Assert.assertEquals(attention, profileAddress.getAttention());
+    }
+
+    @Test
+    public void testGetSequenceNumber() throws Exception {
+        Long sequenceNumber = 11L;
+        profileAddress.setSequenceNumber(sequenceNumber);
+        Assert.assertEquals(sequenceNumber, profileAddress.getSequenceNumber());
+    }
+
+    @Test
+    public void testGetFirstName() throws Exception {
+        String firstName = "first";
+        profileAddress.setFirstName(firstName);
+        Assert.assertEquals(firstName, profileAddress.getFirstName());
+    }
+
+    @Test
+    public void testGetLastName() throws Exception {
+        String lastName = "last";
+        profileAddress.setLastName(lastName);
+        Assert.assertEquals(lastName, profileAddress.getLastName());
+    }
+
+    @Test
+    public void testGetMiddleName() throws Exception {
+        String middleName = "middle";
+        profileAddress.setMiddleName(middleName);
+        Assert.assertEquals(middleName, profileAddress.getMiddleName());
+    }
+
+    @Test
+    public void testGetAddressLine1() throws Exception {
+        String addressLine1 = "postbox";
+        profileAddress.setAddressLine1(addressLine1);
+        Assert.assertEquals(addressLine1, profileAddress.getAddressLine1());
+    }
+
+    @Test
+    public void testGetAddressLine2() throws Exception {
+        String addressLine2 = "AP";
+        profileAddress.setAddressLine2(addressLine2);
+        Assert.assertEquals(addressLine2, profileAddress.getAddressLine2());
+    }
+
+    @Test
+    public void testGetCity() throws Exception {
+        String city = "Hyderabad";
+        profileAddress.setCity(city);
+        Assert.assertEquals(city, profileAddress.getCity());
+    }
+
+    @Test
+    public void testGetState() throws Exception {
+        String state = "AL";
+        profileAddress.setState(state);
+        Assert.assertEquals(state, profileAddress.getState());
+    }
+
+    @Test
+    public void testGetZipCode() throws Exception {
+        Integer zipCode = 32701;
+        profileAddress.setZipCode(zipCode);
+        Assert.assertEquals(zipCode, profileAddress.getZipCode());
+    }
+
+    @Test
+    public void testGetCountryCode() throws Exception {
+        String countryCode = "USA";
+        profileAddress.setCountryCode(countryCode);
+        Assert.assertEquals(countryCode, profileAddress.getCountryCode());
+    }
+
+    @Test
+    public void testGetPrimaryFlag() throws Exception {
+        profileAddress.setPrimaryFlag(true);
+        Assert.assertTrue(profileAddress.getPrimaryFlag());
+    }
+
+    @Test
+    public void testGetEmail() throws Exception {
+        String email = "test1010@blackhole.macys.com";
+        profileAddress.setEmail(email);
+        Assert.assertEquals(email, profileAddress.getEmail());
+    }
+
+    @Test
+    public void testGetBestPhone() throws Exception {
+        profileAddress.setBestPhone("123-444-5577");
+        Assert.assertEquals("223-444-5577", profileAddress.getBestPhone());
+    }
+
+    @Test
+    public void testGetDefaultProfileAddress() throws Exception {
+        ProfileAddress address = ProfileAddress.getDefaultProfileAddress();
+        Assert.assertNotNull(address);
+        Assert.assertNotNull(address.getId());
+        Assert.assertNotNull(address.getAttention());
+        Assert.assertNotNull(address.getSequenceNumber());
+        Assert.assertNotNull(address.getFirstName());
+        Assert.assertNotNull(address.getLastName());
+        Assert.assertNotNull(address.getMiddleName());
+        Assert.assertNotNull(address.getAddressLine1());
+        Assert.assertNotNull(address.getAddressLine2());
+        Assert.assertNotNull(address.getCity());
+        Assert.assertNotNull(address.getState());
+        Assert.assertNotNull(address.getZipCode());
+        Assert.assertNotNull(address.getCountryCode());
+        Assert.assertNotNull(address.getEmail());
+        Assert.assertNotNull(address.getBestPhone());
+        Assert.assertNotNull(address.getPrimaryFlag());
+    }
 }
---
+++ src/test/java/com/macys/sdt/framework/model/RegistryTest.java
@@ -0,0 +1,90 @@
+package com.macys.sdt.framework.model;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RegistryTest {
+
+    private Registry registry=null;
+
+    public RegistryTest()    {
+        registry = new Registry();
+    }
+
+    @Test
+    public void testGetEventType() throws Exception {
+        registry.setEventType("WEDDING");
+        String eventType=registry.getEventType();
+        Assert.assertEquals("WEDDING", eventType);
+    }
+
+    @Test
+    public void testGetEventMonth() throws Exception {
+        registry.setEventMonth("December");
+        String eventMonth=registry.getEventMonth();
+        Assert.assertEquals("December", eventMonth);
+    }
+
+    @Test
+    public void testGetEventDay() throws Exception {
+        registry.setEventDay("17");
+        String eventDay=registry.getEventDay();
+        Assert.assertEquals("17", eventDay);
+    }
+
+    @Test
+    public void testGetEventYear() throws Exception {
+        registry.setEventYear("2016");
+        String eventYear=registry.getEventYear();
+        Assert.assertEquals("2016", eventYear);
+    }
+
+    @Test
+    public void testGetEventLocation() throws Exception {
+        registry.setEventLocation("Alaska");
+        String eventLocation=registry.getEventLocation();
+        Assert.assertEquals("Alaska", eventLocation);
+    }
+
+    @Test
+    public void testGetNumberOfGuest() throws Exception {
+        registry.setNumberOfGuest("110");
+        String numberOfGuest=registry.getNumberOfGuest();
+        Assert.assertEquals("110", numberOfGuest);
+    }
+
+    @Test
+    public void testGetPreferredStoreState() throws Exception {
+        registry.setPreferredStoreState("New York");
+        String preferredStoreState=registry.getPreferredStoreState();
+        Assert.assertEquals("New York", preferredStoreState);
+    }
+
+    @Test
+    public void testGetPreferredStore() throws Exception {
+        registry.setPreferredStore("New York - Herald Square");
+        String preferredStore=registry.getPreferredStore();
+        Assert.assertEquals("New York - Herald Square", preferredStore);
+    }
+
+    @Test
+    public void testGetCoRegistrantFirstName() throws Exception {
+        registry.setCoRegistrantFirstName("JAMES");
+        String coRegistrantFirstName=registry.getCoRegistrantFirstName();
+        Assert.assertEquals("JAMES", coRegistrantFirstName);
+    }
+
+    @Test
+    public void testGetCoRegistrantLastName() throws Exception {
+        registry.setCoRegistrantLastName("SMITH");
+        String coRegistrantLastName=registry.getCoRegistrantLastName();
+        Assert.assertEquals("SMITH", coRegistrantLastName);
+    }
+
+    @Test
+    public void testGetId() throws Exception {
+        registry.setId("1234567");
+        String id=registry.getId();
+        Assert.assertEquals("1234567", id);
+    }
+}
