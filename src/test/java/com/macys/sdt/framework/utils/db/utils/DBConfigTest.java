7ed3faee44ddfd8a3b63b588d5d362c50d3de0f9
Mahendar Doosa <Mahendar.Doosa@macys.com>
2016-12-19 15:35:38 +0530

    B-60214 DBConfig Unit Tests

--- src/test/java/com/macys/sdt/framework/utils/db/utils/DBConfigTest.java
+++ src/test/java/com/macys/sdt/framework/utils/db/utils/DBConfigTest.java
@@ -1,22 +1,95 @@
 package com.macys.sdt.framework.utils.db.utils;
 
-/**
- * Created by M509108 on 11/21/2016.
- */
+import org.junit.Assert;
+import org.junit.Test;
+
 public class DBConfigTest {
 
-   /* @Test
-    public void generateDBUrl() throws Exception {
-        try {
-            DBUtils util = new DBUtils();
-            DBConfig config = util.getConfig();
+    private DBConfig dbConfig;
 
-            Class.forName(config.getDriver());
-            String str = "jdbc:db2://" + config.getHost() + ":" +  config.getPort() + "/" + config.getDbName();
-            Assert.assertEquals(str, config.generateDBUrl());
-        } catch (Exception e) {
-            Assert.fail("fail due to : " + e.getMessage());
-        }
-    }*/
+    public DBConfigTest() {
+        dbConfig = new DBConfig("qa0codemacys", "11.168.114.93", "BLX01", "db2Schema");
+    }
 
+    @Test
+    public void testGetHost() throws Exception {
+        String host = "11.168.112.153";
+        dbConfig.setHost(host);
+        Assert.assertEquals(host, dbConfig.getHost());
+    }
+
+    @Test
+    public void testGetPort() throws Exception {
+        String port = "60000";
+        dbConfig.setPort(port);
+        Assert.assertEquals(port, dbConfig.getPort());
+    }
+
+    @Test
+    public void testGetDbName() throws Exception {
+        String dbName = "MLQ10";
+        dbConfig.setDbName(dbName);
+        Assert.assertEquals(dbName, dbConfig.getDbName());
+    }
+
+    @Test
+    public void testGetUserName() throws Exception {
+        String userName = "mcyapp";
+        dbConfig.setUserName(userName);
+        Assert.assertEquals(userName, dbConfig.getUserName());
+    }
+
+    @Test
+    public void testGetPassword() throws Exception {
+        String password = "dbacce5s";
+        dbConfig.setPassword(password);
+        Assert.assertEquals(password, dbConfig.getPassword());
+    }
+
+    @Test
+    public void testGetEnvName() throws Exception {
+        String envName = "qa0bloomingdales";
+        dbConfig.setEnvName(envName);
+        Assert.assertEquals(envName, dbConfig.getEnvName());
+    }
+
+    @Test
+    public void testGetDBUrl() throws Exception {
+        dbConfig.setPort("60020");
+        dbConfig.setDBUrl(null);
+        Assert.assertEquals("jdbc:db2://11.168.114.93:60020/BLX01", dbConfig.getDBUrl());
+        String dBUrl = "jdbc:db2://11.168.112.101:60000/dbName";
+        dbConfig.setDBUrl(dBUrl);
+        Assert.assertEquals(dBUrl, dbConfig.getDBUrl());
+    }
+
+    @Test
+    public void testGetSchema() throws Exception {
+        String schema = "dbSchema";
+        dbConfig.setSchema(schema);
+        Assert.assertEquals(schema, dbConfig.getSchema());
+    }
+
+    @Test
+    public void testGetDbType() throws Exception {
+        String dbType = "db2";
+        dbConfig.setDbType(dbType);
+        Assert.assertEquals(dbType, dbConfig.getDbType());
+    }
+
+    @Test
+    public void testGenerateDBUrl() throws Exception {
+        String dbName = "MLQ10";
+        String host = "11.168.112.153";
+        String port = "60000";
+
+        DBConfig config = new DBConfig();
+        config.setDbName(dbName);
+        config.setHost(host);
+        config.setPort(port);
+
+        Class.forName(config.getDriver());
+        String str = "jdbc:db2://" + host + ":" + port + "/" + dbName;
+        Assert.assertEquals(str, config.generateDBUrl());
+    }
 }
