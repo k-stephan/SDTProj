62bad92ea9ee0adc69187d2a16d63011935e7400
Rajat Thareja <rajat.thareja@macys.com>
2016-12-20 17:02:50 +0530

    B-60214 WebDriverConfigurator Unit Tests'

--- src/test/java/com/macys/sdt/framework/runner/WebDriverConfiguratorTest.java
+++ src/test/java/com/macys/sdt/framework/runner/WebDriverConfiguratorTest.java
@@ -1,11 +1,13 @@
 package com.macys.sdt.framework.runner;
 
-
-import com.macys.sdt.framework.utils.StepUtils;
 import com.macys.sdt.framework.utils.TestUtils;
-import org.junit.Assert;
-import org.junit.Test;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import net.lightbody.bmp.BrowserMobProxyServer;
+import org.junit.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.util.HashMap;
 
@@ -14,161 +16,144 @@ import java.util.HashMap;
  */
 public class WebDriverConfiguratorTest {
 
-    /*
-    @Test
-    public void testBasicFirefox() {
-        String website = "http://www.macys.com";
-        String browser = "firefox";
-        MainRunner.url = website;
-        MainRunner.browser = browser;
-        DesiredCapabilities caps = WebDriverConfigurator.initBrowserCapabilities();
+    WebDriver driver = null;
 
-        Assert.assertEquals(caps.getBrowserName(), browser);
-        Assert.assertEquals(MainRunner.url, website);
-        Assert.assertTrue(StepUtils.macys());
-        Assert.assertTrue(StepUtils.firefox());
-        Assert.assertFalse(StepUtils.chrome());
-        Assert.assertFalse(StepUtils.iOS());
-        Assert.assertFalse(StepUtils.MEW());
-        Assert.assertFalse(StepUtils.mobileDevice());
-
-        MainRunner.url = null;
+    @After
+    public void reset() throws Exception {
+        try {
+            if (driver != null) {
+                driver.quit();
+                driver = null;
+            }
+        } catch (Exception e) {
+            System.err.println("--> Error - After Test reset driver: " + e.getMessage());
+        }
         MainRunner.browser = null;
-    }
-
-    @Test
-    public void testBasicChromeMEW() {
-        String website = "m.macys.com";
-        String browser = "chrome";
-        String device = "Google Nexus 6";
-        MainRunner.url = website;
-        MainRunner.browser = browser;
-        MainRunner.device = device;
-
-        DesiredCapabilities caps = WebDriverConfigurator.initDeviceCapabilities();
-
-        Assert.assertEquals(caps.getBrowserName(), "android");
-        Assert.assertEquals(MainRunner.url, website);
-        Assert.assertTrue(StepUtils.macys());
-        Assert.assertTrue(StepUtils.chrome());
-        Assert.assertTrue(StepUtils.mobileDevice());
-        Assert.assertFalse(StepUtils.firefox());
-        Assert.assertFalse(StepUtils.iOS());
-
-        MainRunner.url = null;
-        MainRunner.browser = null;
-        MainRunner.device = null;
-    }
-
-    @Test
-    public void testBasicAndroid() {
-        String brand = "mcom";
-        String browser = "chrome";
-        String device = "Google Nexus 6";
-        String osVersion = "6.0";
-        HashMap<String, String> env = new HashMap<>();
-        env.put("appium_server", "127.0.0.1");
-        TestUtils.setEnv(env);
-        MainRunner.browser = browser;
-        MainRunner.device = device;
-        MainRunner.useAppium = true;
-        MainRunner.appLocation = "/path";
-        MainRunner.remoteOS = osVersion;
-        MainRunner.brand = brand;
-
-        DesiredCapabilities caps = WebDriverConfigurator.initDeviceCapabilities();
-
-        Assert.assertEquals(caps.getCapability("platformName"), "Android");
-        Assert.assertEquals(caps.getCapability("platformVersion"), osVersion);
-        Assert.assertEquals(caps.getCapability("deviceName"), device);
-        Assert.assertTrue(StepUtils.macys());
-        Assert.assertTrue(StepUtils.mobileDevice());
-        Assert.assertFalse(StepUtils.iOS());
-
-        MainRunner.browser = null;
-        MainRunner.device = null;
-        MainRunner.useAppium = false;
-        MainRunner.appLocation = null;
         MainRunner.remoteOS = null;
-        MainRunner.brand = null;
+        MainRunner.device = null;
     }
 
     @Test
-    public void testInitBrowserCapabilitiesChrome() {
-        String browser = "chrome";
-        String browserVersion = "54";
-        MainRunner.url = "http://www.macys.com";
-        MainRunner.browser = browser;
-        MainRunner.browserVersion = browserVersion;
-
-        DesiredCapabilities caps = WebDriverConfigurator.initBrowserCapabilities();
-        Assert.assertEquals(caps.getBrowserName(), browser);
-        Assert.assertEquals(caps.getVersion(), browserVersion);
-
-        MainRunner.url = null;
-        MainRunner.browser = null;
-        MainRunner.browserVersion = null;
-    }
-
-    @Test
-    public void testInitBrowserCapabilitiesFirefox() {
-        MainRunner.browser = "";
-        MainRunner.url = "http://www.macys.com";
-
-        DesiredCapabilities caps = WebDriverConfigurator.initBrowserCapabilities();
-        Assert.assertEquals(caps.getBrowserName(), "firefox");
-
-        MainRunner.url = null;
-        MainRunner.browser = null;
-    }
-
-    @Test
-    public void testInitBrowserCapabilitiesIE() {
-        String browserVersion = "10";
-        MainRunner.url = "http://www.macys.com";
-        MainRunner.browser = "ie";
-        MainRunner.browserVersion = browserVersion;
-
-        DesiredCapabilities caps = WebDriverConfigurator.initBrowserCapabilities();
-        Assert.assertEquals(caps.getBrowserName(), "internet explorer");
-        Assert.assertEquals(caps.getVersion(), browserVersion);
-
-        MainRunner.url = null;
-        MainRunner.browser = null;
-        MainRunner.browserVersion = null;
-    }
-
-    @Test
-    public void testInitBrowserCapabilitiesSafari() {
-        String browser = "safari";
-        String browserVersion = "10.0";
-        MainRunner.url = "http://www.macys.com";
-        MainRunner.browser = browser;
-        MainRunner.browserVersion = browserVersion;
-
-        DesiredCapabilities caps = WebDriverConfigurator.initBrowserCapabilities();
-        Assert.assertEquals(caps.getBrowserName(), browser);
-        Assert.assertEquals(caps.getVersion(), browserVersion);
-
-        MainRunner.url = null;
-        MainRunner.browser = null;
-        MainRunner.browserVersion = null;
-    }
-
-    @Test
-    public void testInitBrowserCapabilitiesEdge() {
-        String browserVersion = "20";
-        MainRunner.url = "http://www.macys.com";
+    public void testDefaultBrowserVersion() throws Exception {
+        MainRunner.browser = "firefox";
+        Assert.assertNotNull(WebDriverConfigurator.defaultBrowserVersion());
         MainRunner.browser = "edge";
-        MainRunner.browserVersion = browserVersion;
-
-        DesiredCapabilities caps = WebDriverConfigurator.initBrowserCapabilities();
-        Assert.assertEquals(caps.getBrowserName(), "MicrosoftEdge");
-        Assert.assertEquals(caps.getVersion(), browserVersion);
-
-        MainRunner.url = null;
-        MainRunner.browser = null;
-        MainRunner.browserVersion = null;
+        Assert.assertNotNull(WebDriverConfigurator.defaultBrowserVersion());
+        MainRunner.browser = "ie";
+        Assert.assertNotNull(WebDriverConfigurator.defaultBrowserVersion());
+        MainRunner.browser = "safari";
+        Assert.assertNotNull(WebDriverConfigurator.defaultBrowserVersion());
+        MainRunner.remoteOS = "OS";
+        Assert.assertNotNull(WebDriverConfigurator.defaultBrowserVersion());
+        MainRunner.browser = "chrome";
+        Assert.assertNotNull(WebDriverConfigurator.defaultBrowserVersion());
     }
-    */
+
+    @Test @Ignore("Ignore for Jenkins")
+    public void testInitDriverWithProxy() throws Exception {
+        MainRunner.browser = "firefox";
+        MainRunner.remoteOS = "Windows 7";
+        MainRunner.disableProxy = false;
+        try {
+            driver = WebDriverConfigurator.initDriverWithProxy();
+            Assert.assertNotNull(MainRunner.browsermobServer);
+            Assert.assertTrue(MainRunner.browsermobServer.isStarted());
+            Assert.assertNotNull(MainRunner.browsermobServer.getPort());
+            MainRunner.browsermobServer.stop();
+            Assert.assertTrue(((BrowserMobProxyServer) MainRunner.browsermobServer).isStopped());
+        }
+        catch (Exception e) {
+            Assert.fail("Failed to initialize the driver due to: " +e.getMessage());
+        }
+        finally {
+            MainRunner.disableProxy = true;
+            MainRunner.browsermobServer = null;
+        }
+    }
+
+    @Test @Ignore("Ignore for Jenkins")
+    public void testInitFirefoxDriver() throws Exception {
+        MainRunner.browser = "";
+        MainRunner.remoteOS = "Windows 7";
+        try {
+            driver = WebDriverConfigurator.initDriver(null);
+            Assert.assertEquals(((FirefoxDriver) driver).getCapabilities().getBrowserName(), "firefox");
+        }
+        catch (Exception e){
+            Assert.fail("Failed to initialize the driver due to: " +e.getMessage());
+        }
+    }
+
+    @Test @Ignore("Ignore for Jenkins")
+    public void testInitChromeDriver() throws Exception {
+        String browser = "chrome";
+        MainRunner.browser = browser;
+        MainRunner.remoteOS = "Windows 7";
+        try {
+            driver = WebDriverConfigurator.initDriver(null);
+            Assert.assertEquals(((ChromeDriver) driver).getCapabilities().getBrowserName(), browser);
+        }
+        catch (Exception e) {
+            Assert.fail("Failed to initialize the driver due to: " +e.getMessage());
+        }
+    }
+
+    @Test @Ignore("Ignore for Jenkins")
+    public void testInitInternetExplorerDriver() throws Exception {
+        MainRunner.browser = "ie";
+        MainRunner.remoteOS = "Windows 7";
+        HashMap<String, String> env = new HashMap<>();
+        env.put("HOME", "src/test/java/com/macys/sdt/framework/resources");
+        TestUtils.setEnv(env);
+        try {
+            driver = WebDriverConfigurator.initDriver(null);
+            Assert.assertEquals(((InternetExplorerDriver) driver).getCapabilities().getBrowserName(), "internet explorer");
+        }
+        catch (Exception e) {
+            Assert.fail("Failed to initialize the driver due to: " +e.getMessage());
+        }
+    }
+
+    @Test @Ignore("Ignore for Jenkins")
+    public void testSetupChromeEmulator() throws Exception {
+        MainRunner.useChromeEmulation = true;
+        MainRunner.browser = "chrome";
+        MainRunner.remoteOS = "Windows 7";
+        try {
+            MainRunner.device = "Google Nexus 6";
+            driver = WebDriverConfigurator.initDriver(null);
+            Assert.assertTrue(((ChromeDriver) driver).getCapabilities().is("mobileEmulationEnabled"));
+        }
+        catch (Exception e) {
+            Assert.fail("Failed to initialize the driver due to: " +e.getMessage());
+        }
+        finally {
+            MainRunner.useChromeEmulation = false;
+        }
+    }
+
+    @Test
+    public void testInitSauceLabs() throws Exception {
+        MainRunner.browser = "safari";
+        MainRunner.remoteOS = "OS X 10.11";
+        MainRunner.browserVersion = "10";
+        MainRunner.brand = "MCOM";
+        MainRunner.useSauceLabs = true;
+        MainRunner.sauceUser = "satish-macys";
+        MainRunner.sauceKey = "4fc927f7-c0bd-4f1d-859b-ed3aea2bcc40";
+        try {
+            driver = WebDriverConfigurator.initDriver(null);
+            Assert.assertNotNull(((RemoteWebDriver) driver).getCapabilities().getCapability("webdriver.remote.sessionid"));
+        }
+        catch (Exception e) {
+            Assert.fail("Failed to initialize the driver due to: " +e.getMessage());
+        }
+        finally {
+            MainRunner.useSauceLabs = false;
+            MainRunner.sauceUser = null;
+            MainRunner.sauceKey = null;
+            MainRunner.browserVersion = null;
+            MainRunner.brand = null;
+        }
+    }
 }
