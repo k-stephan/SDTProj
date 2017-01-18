ddcfccca1027d7c430c1065abeb3b4ce0b6edb98
Muneiah Pamujula <muneiah.pamujula@macys.com>
2017-01-18 19:28:55 +0530

    B-60361 :: CBT edge fixes - 0118

--- src/test/java/com/macys/sdt/framework/utils/PageElementTest.java
+++ src/test/java/com/macys/sdt/framework/utils/PageElementTest.java
@@ -38,6 +38,7 @@ public class PageElementTest {
     @Test
     public void testMcomPageElement() throws Exception {
         MainRunner.url = "http://www.qa0codemacys.fds.com";
+        MainRunner.browser = "";
         PageElement testElement = new PageElement("test_page.test_element");
         Assert.assertEquals("test_page", testElement.getPageName());
         Assert.assertEquals("test_element", testElement.getElementName());
