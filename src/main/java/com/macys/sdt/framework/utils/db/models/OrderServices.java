c767ad57f9cc974e781dd7da06b8d41a65b32774
Vikas Katru <vikas.katru@macys.com>
2017-04-28 17:19:32 +0530

    B-75637 : DB Code changes

--- src/main/java/com/macys/sdt/framework/utils/db/models/OrderServices.java
+++ src/main/java/com/macys/sdt/framework/utils/db/models/OrderServices.java
@@ -43,13 +43,166 @@ public class OrderServices {
                 orderDetails.put("ORDER_STATUS", resultSet.getString("ORDER_STATUS"));
                 orderDetails.put("RESERVATION_ID", resultSet.getString("RESERVATION_ID"));
                 orderDetails.put("BASE_FEE", resultSet.getString("BASE_FEE"));
-                orderDetails.put("BASE_FEE", resultSet.getString("ADJUSTED_BASE_FEE"));
+                orderDetails.put("ADJUSTED_BASE_FEE", resultSet.getString("ADJUSTED_BASE_FEE"));
                 orderDetails.put("RESERVATION_STATUS", resultSet.getString("RESERVATION_STATUS"));
             }
         } catch (SQLException | JSONException e) {
             logger.warn("Error in retrieving order details due to : " + e.getMessage());
         }
         return orderDetails;
+    }
+
+    /**
+     * Method to get orderContextInfo from site DB(table:ORDER_CONTEXT_INFO) for a given order number
+     *
+     * @param orderNumber order number to look up for order context info
+     * @return order context info details in hashMap format
+     */
+    public HashMap<String, String> getOrderContextInfo(String orderNumber) {
+        setupConnection();
+        queries = Utils.getSqlQueries();
+        HashMap<String, String> orderContextInfo = new HashMap<>();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(queries.getJSONObject("order_service")
+                    .getString("order_context_info"));
+            preparedStatement.setString(1, orderNumber);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                orderContextInfo.put("DEVICE_INFO", resultSet.getString("DEVICE_INFO"));
+                orderContextInfo.put("CLIENT_ID", resultSet.getString("CLIENT_ID"));
+                orderContextInfo.put("SUB_CLIENT_ID", resultSet.getString("SUB_CLIENT_ID"));
+            }
+        } catch (SQLException e) {
+            logger.warn("Error in retrieving order context info due to : " + e.getMessage());
+        }
+        return orderContextInfo;
+    }
+
+    /**
+     * Method to get order attributes from site DB(table:ORDER_ATTRIBUTES) for a given order number
+     *
+     * @param orderNumber order number to look up for order attributes
+     * @return order attributes in hashMap format
+     */
+    public HashMap<String, String> getAllOrderAttributes(String orderNumber) {
+        setupConnection();
+        queries = Utils.getSqlQueries();
+        HashMap<String, String> orderAttributes = new HashMap<>();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(queries.getJSONObject("order_service")
+                    .getString("order_attributes"));
+            preparedStatement.setString(1, orderNumber);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                orderAttributes.put(resultSet.getObject(1).toString(), resultSet.getObject(2).toString());
+            }
+        } catch (SQLException e) {
+            logger.warn("Error in retrieving order attributes due to : " + e.getMessage());
+        }
+        return orderAttributes;
+    }
+
+    /**
+     * Method to get EPS authorization request from site DB(table:EPS_LOG) for a given order number
+     *
+     * @param orderNumber order number to look up for EPS authorization request
+     * @param paymentType mode of payment for the order (paypal/creditcard)
+     * @return EPS authorization request in Element format
+     */
+    public Element getEpsAuthorizationRequest(String orderNumber, String paymentType) {
+        setupConnection();
+        queries = Utils.getSqlQueries();
+        Element epsAuthorizationRequest = null;
+        String serviceName = null;
+        switch (paymentType.toLowerCase()) {
+            case "paypal":
+                serviceName = "eps_paypal_authorization_request";
+                break;
+            case "creditcard":
+            case "credit card":
+                serviceName = "eps_authorization_request";
+                break;
+            default:
+                throw new IllegalArgumentException("Unexpected payment type given: " + paymentType);
+        }
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(queries.getJSONObject("order_service").getString(serviceName));
+            preparedStatement.setString(1, orderNumber);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                String xmlData = resultSet.getString("XML_DATA");
+                epsAuthorizationRequest = getXmlElements(xmlData);
+            }
+        } catch (SQLException e) {
+            logger.warn("Error in retrieving eps authorization request due to : " + e.getMessage());
+        }
+        return epsAuthorizationRequest;
+    }
+
+    /**
+     * Method to get EPS authorization response from site DB(table:EPS_LOG) for a given order number
+     *
+     * @param orderNumber order number to look up for EPS authorization response
+     * @param paymentType mode of payment for the order (paypal/creditcard)
+     * @return EPS authorization response in Element format
+     */
+    public Element getEpsAuthorizationResponse(String orderNumber, String paymentType) {
+        setupConnection();
+        queries = Utils.getSqlQueries();
+        Element epsAuthorizationResponse = null;
+        String serviceName = null;
+        switch (paymentType.toLowerCase()) {
+            case "paypal":
+                serviceName = "eps_paypal_authorization_response";
+                break;
+            case "creditcard":
+            case "credit card":
+                serviceName = "eps_authorization_response";
+                break;
+            default:
+                throw new IllegalArgumentException("Unexpected payment type given: " + paymentType);
+        }
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(queries.getJSONObject("order_service").getString(serviceName));
+            preparedStatement.setString(1, orderNumber);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                epsAuthorizationResponse = getXmlElements(resultSet.getString("XML_DATA"));
+            }
+        } catch (SQLException e) {
+            logger.warn("Error in retrieving eps authorization response due to : " + e.getMessage());
+        }
+        return epsAuthorizationResponse;
+    }
+
+    /**
+     * Method to get all the shipments of an order from site DB(TABLE:SHIPMENT) for a given order number
+     *
+     * @param orderNumber order number to get shipments
+     * @return List of shipments in HashMap format
+     **/
+    public List<HashMap> getShipments(String orderNumber) {
+        setupConnection();
+        List<HashMap> shipments = new ArrayList<>();
+        queries = Utils.getSqlQueries();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    queries.getJSONObject("order_service").getString("order_shipment_details"));
+            preparedStatement.setString(1, orderNumber);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                HashMap<String, String> shipment = new HashMap<>();
+                shipment.put("SHIPMENT_ID", resultSet.getString("SHIPMENT_ID"));
+                shipment.put("SHIP_METHOD_CODE", resultSet.getString("SHIP_METHOD_CODE"));
+                shipment.put("SHIP_CONTACT_ID", resultSet.getString("SHIP_CONTACT_ID"));
+                shipment.put("REGISTRY_ID", resultSet.getString("REGISTRY_ID"));
+                shipment.put("REGISTRY_ADDR_USED_F", resultSet.getString("REGISTRY_ADDR_USED_F"));
+                shipments.add(shipment);
+            }
+        } catch (SQLException e) {
+            logger.warn("Error in retrieving shipments due to : " + e.getMessage());
+        }
+        return shipments;
     }
 
     /**
