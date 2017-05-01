221bb183839b7fef8618a824d556204991af1053
Taylor Hutchinson <Taylor.Hutchinson@macys.com>
2017-05-01 15:21:47 -0700

    Adding ShopAppClient B-60214

--- src/main/java/com/macys/sdt/framework/exceptions/SkipException.java
+++ src/main/java/com/macys/sdt/framework/exceptions/SkipException.java
@@ -3,7 +3,7 @@ package com.macys.sdt.framework.exceptions;
 /**
  * Exception to throw to skip a step
  */
-public class SkipException extends Exception {
+public class SkipException extends RuntimeException {
     private static final long serialVersionUID = 3719597044976490615L;
 
     public SkipException(String msg) {
