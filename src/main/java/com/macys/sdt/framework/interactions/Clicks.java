250cf378d2d9d80bdcdf205177c5bfa9f1e6a057
stephenk-QA <65146566+stephenk-phxa@users.noreply.github.com>
2024-07-31 04:18:47 -0700

    SDT Commit

--- src/main/java/com/macys/sdt/framework/interactions/Clicks.java
+++ src/main/java/com/macys/sdt/framework/interactions/Clicks.java
@@ -15,7 +15,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.function.Predicate;
 
 import static com.macys.sdt.framework.runner.RunConfig.appTest;
